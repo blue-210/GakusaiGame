75c74910faeb18945f1a23d32f51fdc85b9237cb
blue-210 <blue210@MacBook-Air.local>
2015-07-19 23:13:05 +0900

    おみくじ画面をセンタリング

--- omikuji/omikuji.js
+++ omikuji/omikuji.js
@@ -1,11 +1,10 @@
+$(function(){
+   $('#getOmikuji').click(getOmikuji);
 
-window.onload = function(){
-   document.getElementById('getOmikuji').onclick = getOmikuji;
-}
+   function getOmikuji(){
+      var omikuji = ["大吉","中吉","小吉","末吉","凶","大凶"];
+      var result = Math.floor(Math.random() * omikuji.length);
 
-function getOmikuji(){
-   var omikuji = ["大吉","中吉","小吉","末吉","凶","大凶"];
-   var result = Math.floor(Math.random() * omikuji.length);
-
-   document.getElementById('result').innerHTML = omikuji[result];
-}
+      $('#result').text(omikuji[result]);
+   }
+});
