f2c00cf755b621734a714bc9f514828d3b54b0fc
blue-210 <j5hfca7pm@gmail.com>
2015-07-21 19:49:32 +0900

    JNDIがうまく動かないバグの修正（途中）

--- WEB-INF/src/test/H2Test.java
+++ WEB-INF/src/test/H2Test.java
@@ -5,20 +5,22 @@ import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.DriverManager;
 
 public class H2Test{
    public static void main(String[] args) {
       Connector cn = new H2Connector();
       Connection conn = cn.getConnection();
-      String sql = "SELECT * FROM touchranking";
 
       try{
+         String sql = "SELECT * FROM diffranking";
+
          PreparedStatement stm = conn.prepareStatement(sql);
 
          ResultSet rs = stm.executeQuery();
 
          while(rs.next()){
-            System.out.println(rs.next());
+            System.out.println(rs.getString("rank")+" "+rs.getString("score"));
          }
       }catch(SQLException e){
          e.printStackTrace();
